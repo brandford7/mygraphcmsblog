799d36f32f5ed9200f7a4e3130711e145058ca60
brandford7 <brandfordjunior@gmail.com>
2022-01-13 11:43:02 +0000

    initial commit

---
+++ Components/PostCard.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const PostCard = () => {
+  return (
+    <>
+      <Box></Box>
+    </>
+  );
+};
+
+export default PostCard;
