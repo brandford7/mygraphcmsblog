755ce9800358bc635fbdb6b2b6778832012a93ad
brandford7 <brandfordjunior@gmail.com>
2022-01-23 00:44:21 +0000

    new change

--- sections/AdjacentPosts.jsx
+++ sections/AdjacentPosts.jsx
@@ -16,7 +16,7 @@ const AdjacentPosts = ({ createdAt, slug }) => {
 
   return (
     <>
-      <div className="grid grid-cols-1 lg:grid-cols-8 gap-12 ">
+      <div className="grid grid-cols-1 lg:grid-cols-8 gap-12 mb-8 ">
         {dataLoaded && (
           <>
             {adjacentPost.previous && (
