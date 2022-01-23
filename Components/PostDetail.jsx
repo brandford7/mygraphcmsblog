a6b352b6b22b48b267f14338d6fbd64acbb25491
brandford7 <brandfordjunior@gmail.com>
2022-01-23 00:01:05 +0000

    next/image

--- Components/PostDetail.jsx
+++ Components/PostDetail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import Image from "next/image";
 import moment from "moment";
 import { Box, chakra, Img, Text } from "@chakra-ui/react";
 
@@ -20,45 +20,45 @@ const PostDetail = ({ post }) => {
         modifiedText = <u key={index}>{text}</u>;
       }
     }
- switch (type) {
-   case "heading-three":
-     return (
-       <h3 key={index} className="text-xl font-semibold mb-4">
-         {modifiedText.map((item, i) => (
-           <React.Fragment key={i}>{item}</React.Fragment>
-         ))}
-       </h3>
-     );
-   case "paragraph":
-     return (
-       <p key={index} className="mb-8">
-         {modifiedText.map((item, i) => (
-           <React.Fragment key={i}>{item}</React.Fragment>
-         ))}
-       </p>
-     );
-   case "heading-four":
-     return (
-       <h4 key={index} className="text-md font-semibold mb-4">
-         {modifiedText.map((item, i) => (
-           <React.Fragment key={i}>{item}</React.Fragment>
-         ))}
-       </h4>
-     );
-   case "image":
-     return (
-       <img
-         key={index}
-         alt={obj.title}
-         height={obj.height}
-         width={obj.width}
-         src={obj.src}
-       />
-     );
-   default:
-     return modifiedText;
- }
-     };
+    switch (type) {
+      case "heading-three":
+        return (
+          <h3 key={index} className="text-xl font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h3>
+        );
+      case "paragraph":
+        return (
+          <p key={index} className="mb-8">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </p>
+        );
+      case "heading-four":
+        return (
+          <h4 key={index} className="text-md font-semibold mb-4">
+            {modifiedText.map((item, i) => (
+              <React.Fragment key={i}>{item}</React.Fragment>
+            ))}
+          </h4>
+        );
+      case "image":
+        return (
+          <img
+            key={index}
+            alt={obj.title}
+            height={obj.height}
+            width={obj.width}
+            src={obj.src}
+          />
+        );
+      default:
+        return modifiedText;
+    }
+  };
 
   return (
     <>
@@ -93,7 +93,7 @@ const PostDetail = ({ post }) => {
               w={{ lg: "auto" }}
               mr="8"
             >
-              <Img
+              <Image
                 alt={post.author.name}
                 height="30px"
                 width="30px"
