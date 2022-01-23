a0568a5cad213f56b26b8cfa2765fcff079b6ad8
brandford7 <brandfordjunior@gmail.com>
2022-01-23 00:16:42 +0000

    changes

--- Components/AdjacentPostCard.jsx
+++ Components/AdjacentPostCard.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import moment from "moment";
 import Link from "next/link";
-import { Box } from "@chakra-ui/react";
 
 const AdjacentPostCard = ({ post, position }) => (
   <>
-    <Box
-      pos="absolute" rounded="lg" bgPosition="center" bgRepeat="no-repeat" bgSize="cover" shadow="md" display="inline-block" w="full" h="72" mb="8"
+    <div
+      className="absolute rounded-lg bg-center bg-no-repeat bg-cover shadow-md inline-block w-full h-72"
       style={{ backgroundImage: `url('${post.featuredImage.url}')` }}
     />
     <div className="absolute rounded-lg bg-center bg-gradient-to-b opacity-50 from-gray-400 via-gray-700 to-black w-full h-72" />
